041caa8cbbe3103c168c0068a2f2fb7f3f4ad5b8
Karrew9 <167529507+Karrew9@users.noreply.github.com>
2024-10-01 13:31:54 +0300

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+    public class Circle : IShape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Радиус должен быть положительным числом.");
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    public class Triangle : IShape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Стороны треугольника должны быть положительными числами.");
+
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+                throw new ArgumentException("Треугольник с такими сторонами не существует.");
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = new double[] { SideA, SideB, SideC };
+            Array.Sort(sides);
+            return Math.Abs(sides[0] * sides[0] + sides[1] * sides[1] - sides[2] * sides[2]) < 1e-6;
+        }
+    }
+    public static class ShapeFactory
+    {
+        public static IShape CreateCircle(double radius)
+        {
+            return new Circle(radius);
+        }
+
+        public static IShape CreateTriangle(double sideA, double sideB, double sideC)
+        {
+            return new Triangle(sideA, sideB, sideC);
+        }
+    }
+}
